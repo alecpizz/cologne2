7cd7f84681e90171d4c2ce059aeb7a5e99f88dc0
Alec Pizziferro <apizziferro@mail.bradley.edu>
2025-07-16 22:38:25 -0500

    switch to squish library. kinda working!

--- src/engine/renderer/types/Texture.cpp
+++ src/engine/renderer/types/Texture.cpp
@@ -5,17 +5,17 @@
 
 #include "Texture.h"
 
-#include <stb_dxt/stb_dxt.h>
 #include <fstream>
 #include <engine/util/FileUtil.h>
 #include <filesystem>
+#include <squish/squish.h>
 
 namespace cologne
 {
-
 #define FOURCC_DXT1 0x31545844 // Equivalent to "DXT1"
 
-    struct DDS_PIXELFORMAT {
+    struct DDS_PIXELFORMAT
+    {
         uint32_t dwSize;
         uint32_t dwFlags;
         uint32_t dwFourCC;
@@ -26,21 +26,22 @@ namespace cologne
         uint32_t dwABitMask;
     };
 
-    struct DDS_HEADER {
-        uint32_t           dwSize;
-        uint32_t           dwFlags;
-        uint32_t           dwHeight;
-        uint32_t           dwWidth;
-        uint32_t           dwPitchOrLinearSize;
-        uint32_t           dwDepth;
-        uint32_t           dwMipMapCount;
-        uint32_t           dwReserved1[11];
-        DDS_PIXELFORMAT    ddspf;
-        uint32_t           dwCaps;
-        uint32_t           dwCaps2;
-        uint32_t           dwCaps3;
-        uint32_t           dwCaps4;
-        uint32_t           dwReserved2;
+    struct DDS_HEADER
+    {
+        uint32_t dwSize;
+        uint32_t dwFlags;
+        uint32_t dwHeight;
+        uint32_t dwWidth;
+        uint32_t dwPitchOrLinearSize;
+        uint32_t dwDepth;
+        uint32_t dwMipMapCount;
+        uint32_t dwReserved1[11];
+        DDS_PIXELFORMAT ddspf;
+        uint32_t dwCaps;
+        uint32_t dwCaps2;
+        uint32_t dwCaps3;
+        uint32_t dwCaps4;
+        uint32_t dwReserved2;
     };
 
 
@@ -60,7 +61,8 @@ namespace cologne
             LOG_ERROR("Failed to load texture from");
             LOG_ERROR(texture_path);
             glDeleteTextures(1, &_handle);
-        } else
+        }
+        else
         {
             _width = width;
             _height = height;
@@ -83,7 +85,8 @@ namespace cologne
         if (height == 0)
         {
             _data = std::vector<unsigned char>(data, data + width);
-        } else
+        }
+        else
         {
             _data = std::vector<unsigned char>(data, data + (width * height));
         }
@@ -161,8 +164,8 @@ namespace cologne
         stbi_set_flip_vertically_on_load(false);
         int new_width = 0, new_height = 0, new_channels = 0;
         stbi_uc *img_data =
-            stbi_load_from_memory(_data.data(), static_cast<int>(_data.size()),
-                &new_width, &new_height, &new_channels, 0);
+                stbi_load_from_memory(_data.data(), static_cast<int>(_data.size()),
+                                      &new_width, &new_height, &new_channels, 0);
         _width = new_width;
         _height = new_height;
         _channels = new_channels;
@@ -179,11 +182,13 @@ namespace cologne
         {
             format = GL_RED;
             format_internal = GL_R8;
-        } else if (_channels == 3)
+        }
+        else if (_channels == 3)
         {
             format = GL_RGB;
             format_internal = GL_RGB8;
-        } else if (_channels == 4)
+        }
+        else if (_channels == 4)
         {
             format = GL_RGBA;
             format_internal = GL_RGBA8;
@@ -204,7 +209,7 @@ namespace cologne
         {
             return;
         }
-        std::filesystem::path f_path (_path);
+        std::filesystem::path f_path(_path);
         if (_path.length() > 512)
         {
             LOG_WARN("PATH TOO LONG, SKIPPING");
@@ -232,7 +237,7 @@ namespace cologne
             return;
         }
 
-        std::ifstream file (_path, std::ios::binary);
+        std::ifstream file(_path, std::ios::binary);
         if (file.fail())
         {
             LOG_ERROR("Couldn't open import file %s %s", _path.c_str(), strerror(errno));
@@ -244,7 +249,7 @@ namespace cologne
         // outfile.write(reinterpret_cast<const char*>(compressed_data.data()), compressed_data.size());
         // outfile.close();
         char sign[4];
-        file.read(reinterpret_cast<char*>(&sign), 4);
+        file.read(reinterpret_cast<char *>(&sign), 4);
         if (!strcmp(sign, "DDS "))
         {
             LOG_ERROR("File not DDS!, got: %s", sign);
@@ -252,13 +257,13 @@ namespace cologne
         }
 
         DDS_HEADER header = {};
-        file.read(reinterpret_cast<char*>(&header), sizeof(header));
+        file.read(reinterpret_cast<char *>(&header), sizeof(header));
         int new_width = header.dwWidth;
-        int new_height= header.dwHeight;
+        int new_height = header.dwHeight;
         int channels = 4;
         int compressed_size = header.dwPitchOrLinearSize;
         std::vector<uint8_t> compressed_data(compressed_size);
-        file.read(reinterpret_cast<char*>(compressed_data.data()), compressed_data.size());
+        file.read(reinterpret_cast<char *>(compressed_data.data()), compressed_data.size());
         file.close();
 
         _width = new_width;
@@ -276,7 +281,8 @@ namespace cologne
 
         int32_t mips = floor(log2(std::max(_width, _height))) + 1;
         glTextureStorage2D(_handle, mips, format_internal, _width, _height);
-        glCompressedTextureSubImage2D(_handle, 0, 0, 0, _width, _height, format_internal, compressed_size, compressed_data.data());
+        glCompressedTextureSubImage2D(_handle, 0, 0, 0, _width, _height, format_internal, compressed_size,
+                                      compressed_data.data());
         glGenerateTextureMipmap(_handle);
         _data.clear();
         _data.shrink_to_fit();
@@ -311,7 +317,7 @@ namespace cologne
         _is_resident = false;
     }
 
-    void Texture::export_to_compressed(const char* path) const
+    void Texture::export_to_compressed(const char *path) const
     {
         if (_data.empty())
         {
@@ -322,8 +328,8 @@ namespace cologne
         int new_height;
         int new_channels;
         stbi_uc *img_data =
-           stbi_load_from_memory(_data.data(), static_cast<int>(_data.size()),
-               &new_width, &new_height, &new_channels, 4);
+                stbi_load_from_memory(_data.data(), static_cast<int>(_data.size()),
+                                      &new_width, &new_height, &new_channels, 4);
         if (new_width % 4 != 0 || new_height % 4 != 0)
         {
             //TODO: handle this
@@ -333,33 +339,8 @@ namespace cologne
         }
 
         int compressed_size = (new_width * new_height) / 2;
-        // std::vector<uint8_t> compressed_data(compressed_size);
-        // for (int y = 0; y < new_height; y += 4)
-        // {
-        //     for (int x = 0; x < new_width; x += 4)
-        //     {
-        //         int block_x = x / 4;
-        //         int block_y = y / 4;
-        //         int blocks_per_row = new_width / 4;
-        //         uint8_t* dest_block = &compressed_data[(block_y * blocks_per_row + block_x) * 8];
-        //
-        //         uint8_t source_block[16 * 4];
-        //
-        //         for (int row_in_block = 0; row_in_block < 4; row_in_block++)
-        //         {
-        //             uint8_t* source_row = img_data + ((y + row_in_block) * new_width + x) * 4;
-        //
-        //             uint8_t* dest_row = img_data + (row_in_block * 4 * 4);
-        //
-        //             memcpy(dest_row, source_row, 16);
-        //         }
-        //
-        //         stb_compress_dxt_block(dest_block, source_block, 1, STB_DXT_HIGHQUAL);
-        //     }
-        // }
-        stbi_image_free(img_data);
         FileUtil::create_directory_recursive(path);
-        std::ofstream outfile (path, std::ios::binary);
+        std::ofstream outfile(path, std::ios::binary);
         if (outfile.fail())
         {
             LOG_ERROR("Couldn't open output file %s %s", path, strerror(errno));
@@ -381,34 +362,16 @@ namespace cologne
         header.dwCaps = 0x1000; // TEXTURE
 
         outfile.write("DDS ", 4);
-        outfile.write(reinterpret_cast<const char*>(&header), sizeof(header));
+        outfile.write(reinterpret_cast<const char *>(&header), sizeof(header));
 
-        unsigned char inDataBuf[64];
-        unsigned char outDataBuf[16];
+        int cFlags = squish::kDxt1;
+        int cSize = squish::GetStorageRequirements(new_width, new_height, cFlags);
+        std::vector<uint8_t> pixels(cSize);
+        squish::CompressImage(img_data, new_width, new_height, new_width * 4, pixels.data(), cFlags);
 
-        const int blockSize = 16;
-        const int mipCount = 1;
-        int mw = new_width, mh = new_height;
-        for (unsigned int m = 0; m < mipCount; ++m, mw /= 2, mh /= 2)
-        {
-            for (int l = 0; l < mh; l += 4, img_data += mw * 4 * 3)    /* every 4 rows */
-            {
-                for (int j = 0; j < mw; j += 4, img_data += 4 * 4)     /* every 4 columns */
-                {
-                    for (int i = 0; i < 4; ++i)     /* every row in the 4x4 RGBA pixel block */
-                    {
-                        for (int k = 0; k < 4; ++k)
-                        {
-                            memcpy(inDataBuf + (i * 4 + k) * 4, img_data + (i * mw + k) * 4, 4);
-                        }
-                    }
-                    stb_compress_dxt_block(outDataBuf, inDataBuf, 1,  STB_DXT_HIGHQUAL);
-                    outfile.write(reinterpret_cast<const char *>(&outDataBuf), blockSize);
-                }
-            }
-        }
+        outfile.write(reinterpret_cast<const char *>(pixels.data()), pixels.size());
 
-
+        stbi_image_free(img_data);
         outfile.close();
     }
 }
--- vendor/stb_dxt/src/stb_dxt.cpp
+++
@@ -1,2 +0,0 @@
-#define STB_DXT_IMPLEMENTATION
-#include <stb_dxt/stb_dxt.h>
