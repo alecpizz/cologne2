be18d6d9af2b55d079b211c825b219b89c836012
Alec Pizziferro <apizziferro@mail.bradley.edu>
2025-07-21 00:54:45 -0500

    test scene save

--- src/engine/scene/SceneSaver.cpp
+++ src/engine/scene/SceneSaver.cpp
@@ -11,6 +11,75 @@
 #include <fstream>
 #include <nlohmann/json.hpp>
 
+namespace nlohmann
+{
+    template<>
+    struct adl_serializer<glm::vec2>
+    {
+        static void to_json(json& j, const glm::vec2& vec)
+        {
+            j = {vec.x, vec.y};
+        }
+
+        static void from_json(const json& j, glm::vec2& vec)
+        {
+            j.at(0).get_to(vec.x);
+            j.at(1).get_to(vec.y);
+        }
+    };
+
+    template<>
+    struct adl_serializer<glm::vec3>
+    {
+        static void to_json(json& j, const glm::vec3& vec)
+        {
+            j = {vec.x, vec.y, vec.z};
+        }
+
+        static void from_json(const json& j, glm::vec3& vec)
+        {
+            j.at(0).get_to(vec.x);
+            j.at(1).get_to(vec.y);
+            j.at(2).get_to(vec.z);
+        }
+    };
+
+    template<>
+    struct adl_serializer<glm::vec4>
+    {
+        static void to_json(json& j, const glm::vec4& vec)
+        {
+            j = {vec.x, vec.y, vec.z, vec.w};
+        }
+
+        static void from_json(const json& j, glm::vec4& vec)
+        {
+            j.at(0).get_to(vec.x);
+            j.at(1).get_to(vec.y);
+            j.at(2).get_to(vec.z);
+            j.at(3).get_to(vec.w);
+        }
+    };
+
+    template<>
+    struct adl_serializer<glm::quat>
+    {
+        static void to_json(json& j, const glm::quat& quat)
+        {
+            j = {quat.w, quat.x, quat.y, quat.z};
+        }
+
+        static void from_json(const json& j, glm::quat& quat)
+        {
+            j.at(0).get_to(quat.w);
+            j.at(1).get_to(quat.x);
+            j.at(2).get_to(quat.y);
+            j.at(3).get_to(quat.z);
+        }
+    };
+}
+
+
 namespace cologne
 {
     SceneSaver::SceneSaver(Scene *scene)
@@ -20,8 +89,13 @@ namespace cologne
 
     static void serialize_entity(nlohmann::json& json, Entity entity)
     {
-        json["Entity"] = static_cast<uint32_t>(entity);
-        json["TagComponent"]["Tag"] = entity.get_name();
+        json["Entity"] = static_cast<uint64_t>(entity.get_uuid());
+        json["TagComponent"]["tag"] = entity.get_name();
+
+        auto& tr = entity.get_transform();
+        json["TransformComponent"]["position"] = tr.position;
+        json["TransformComponent"]["rotation"] = tr.rotation;
+        json["TransformComponent"]["scale"] = tr.scale;
     }
 
     void SceneSaver::serialize(const std::string &path)
