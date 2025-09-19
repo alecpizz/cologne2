6f628ea4e1774241eec018980fe24ad08e7586e3
Alec Pizziferro <apizziferro@mail.bradley.edu>
2025-09-18 22:47:46 -0500

    actual fix the scale problems this time

--- src/engine/scene/systems/RagdollSystem.cpp
+++ src/engine/scene/systems/RagdollSystem.cpp
@@ -4,6 +4,7 @@
 
 #include "RagdollSystem.h"
 #include <engine/asset_manager/AssetManager.h>
+#include <engine/core/Input.h>
 #include <engine/scene/Components.h>
 #include <engine/scene/Entity.h>
 #include <engine/scene/Scene.h>
@@ -12,11 +13,11 @@ namespace cologne
 {
     void RagdollSystem::on_scene_start(Scene *scene)
     {
-        auto& registry = scene->get_raw_registry();
-        for (const auto entity : registry.view<RagdollComponent, SkinnedModelComponent>())
+        auto &registry = scene->get_raw_registry();
+        for (const auto entity: registry.view<RagdollComponent, SkinnedModelComponent>())
         {
-            auto& rd = registry.get<RagdollComponent>(entity);
-            auto& sm = registry.get<SkinnedModelComponent>(entity);
+            auto &rd = registry.get<RagdollComponent>(entity);
+            auto &sm = registry.get<SkinnedModelComponent>(entity);
             auto model = AssetManager::get_skinned_model_by_name(sm.model_name);
             if (!model)
             {
@@ -34,7 +35,7 @@ namespace cologne
             }
             pose.update_skinning_matrices(sm.skeleton);
             rd.id = Physics::create_ragdoll({entity, scene},
-                rd.bone_to_ragdoll_map, sm.skeleton, pose.global_transforms);
+                                            rd.bone_to_ragdoll_map, sm.skeleton, pose.global_transforms);
             if (rd.current_state == RagdollComponent::State::KINEMATIC)
             {
                 Physics::make_ragdoll_kinematic(rd.id);
@@ -44,17 +45,18 @@ namespace cologne
 
     void RagdollSystem::on_update(Scene *scene, float dt)
     {
-        auto& registry = scene->get_raw_registry();
-        auto ragdolls = registry.view<SkinnedModelComponent, RagdollComponent, ActiveComponent, WorldTransformComponent>();
-        for (auto entity : ragdolls)
+        auto &registry = scene->get_raw_registry();
+        auto ragdolls = registry.view<SkinnedModelComponent, RagdollComponent, ActiveComponent,
+            WorldTransformComponent>();
+        for (auto entity: ragdolls)
         {
             if (!registry.get<ActiveComponent>(entity).active)
             {
                 continue;
             }
-            auto& transform = registry.get<WorldTransformComponent>(entity).transform;
-            auto& sm = registry.get<SkinnedModelComponent>(entity);
-            auto& rd = registry.get<RagdollComponent>(entity);
+            auto &transform = registry.get<WorldTransformComponent>(entity).transform;
+            auto &sm = registry.get<SkinnedModelComponent>(entity);
+            auto &rd = registry.get<RagdollComponent>(entity);
             if (rd.id == UINT32_MAX)
             {
                 continue;
@@ -74,37 +76,58 @@ namespace cologne
             {
                 glm::mat4 inverse_entity_transform = glm::inverse(transform);
 
+                std::vector<glm::mat4> stable_global_transforms(sm.skeleton.get_bone_count());
+                for (int i = 0; i < sm.skeleton.get_bone_count(); ++i)
+                {
+                    const auto &bone = sm.skeleton.get_bones()[i];
+                    glm::mat4 parent_transform = (bone.parent_idx == -1)
+                                                     ? glm::mat4(1.0f)
+                                                     : stable_global_transforms[bone.parent_idx];
+                    stable_global_transforms[i] = parent_transform * bone.local_bind_transform;
+                }
+
                 for (int i = 0; i < sm.skeleton.get_bone_count(); i++)
                 {
                     const auto &bone = sm.skeleton.get_bones()[i];
-                    std::string node_name = bone.name;
-                    glm::mat4 node_transform = sm.skeleton_pose.local_transforms[i];
-                    unsigned int parent_idx = bone.parent_idx;
-                    glm::mat4 parent_transform = (parent_idx == -1) ? glm::mat4(1.0f) : sm.skeleton_pose.global_transforms[parent_idx];
-                    glm::mat4 animated_global_transform = parent_transform * node_transform;
-                    glm::mat4 final_transform;
+                    const std::string &node_name = bone.name;
+
+                    glm::mat4 final_global_transform;
 
                     if (rd.bone_to_ragdoll_map.contains(node_name))
                     {
-                        glm::vec3 animated_scale;
+                        glm::mat4 stable_transform = stable_global_transforms[i];
+
+                        glm::vec3 correct_scale;
                         glm::vec3 dummy_pos;
+                        glm::vec3 dummy_scale;
                         glm::quat dummy_rot;
-                        Util::decompose_mat4(animated_global_transform, dummy_pos, dummy_rot, animated_scale);
-                        glm::mat4 rigidbody_world_transform = Physics::get_rigidbody_transform(rd.bone_to_ragdoll_map[node_name]);
-                        glm::mat4 scaled_rigidbody_world_transform = rigidbody_world_transform * glm::scale(glm::mat4(1.0f), animated_scale);
-                        final_transform = inverse_entity_transform * scaled_rigidbody_world_transform;
+                        Util::decompose_mat4(stable_transform, dummy_pos, dummy_rot, correct_scale);
+
+                        glm::mat4 rigidbody_world_transform = Physics::get_rigidbody_transform(
+                            rd.bone_to_ragdoll_map.at(node_name));
+
+                        Util::decompose_mat4(rigidbody_world_transform, dummy_pos, dummy_rot, dummy_scale);
+                        glm::mat4 unscaled_rigidbody_transform =
+                                glm::translate(glm::mat4(1.0f), dummy_pos) * glm::toMat4(dummy_rot);
+                        glm::mat4 scaled_rigidbody_world_transform = glm::scale(
+                            unscaled_rigidbody_transform, correct_scale);
+
+                        final_global_transform = inverse_entity_transform * scaled_rigidbody_world_transform;
                     }
                     else
                     {
-                        final_transform = animated_global_transform;
+
+                        glm::mat4 parent_transform = bone.parent_idx == -1
+                                                         ? glm::mat4(1.0f)
+                                                         : sm.skeleton_pose.global_transforms[bone.parent_idx];
+                        final_global_transform = parent_transform * bone.local_bind_transform;
                     }
 
-                    sm.skeleton_pose.global_transforms[i] = final_transform;
+                    sm.skeleton_pose.global_transforms[i] = final_global_transform;
                 }
+
                 sm.skeleton_pose.update_skinning_matrices_no_rebuild(sm.skeleton);
             }
         }
     }
-
-
 }
