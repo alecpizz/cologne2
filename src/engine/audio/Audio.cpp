38a2bbada8b92944001e3aa542ed6efe5fc5712a
Alec Pizziferro <apizziferro@mail.bradley.edu>
2025-10-19 15:35:35 -0500

    nuke sdl mixer

--- src/engine/audio/Audio.cpp
+++ src/engine/audio/Audio.cpp
@@ -5,97 +5,96 @@
 #include "Audio.h"
 
 #include <engine/util/DebugScope.h>
-#include <SDL3_mixer/SDL_mixer.h>
 
 namespace cologne::Audio
 {
-    std::unordered_map<std::string, Mix_Music*> musics;
-    std::unordered_map<std::string, Mix_Chunk*> sounds;
+    // std::unordered_map<std::string, Mix_Music*> musics;
+    // std::unordered_map<std::string, Mix_Chunk*> sounds;
     void init()
     {
         LOG_INFO("AUDIO INTI");
         DebugScope scope("Audio::init");
-        if (!Mix_OpenAudio(0, nullptr))
-        {
-            LOG_ERROR("Failed to start SDL_MIXER! Error: %s", SDL_GetError());
-        }
-        else
-        {
-            LOG_INFO("Audio Mixer started succesfully");
-        }
+        LOG_ERROR("Audio not implemented");
     }
 
     void add_sound(const char *sound_path)
     {
-        if (sounds.contains(sound_path))
-        {
-            return;
-        }
-        auto s = Mix_LoadWAV(sound_path);
-        if (s == nullptr)
-        {
-            LOG_ERROR("Failed to load sound file: %s", SDL_GetError());
-        }
-        sounds.insert(std::pair<std::string, Mix_Chunk*>(sound_path, s));
+        LOG_ERROR("Audio not implemented");
+        // if (sounds.contains(sound_path))
+        // {
+        //     return;
+        // }
+        // auto s = Mix_LoadWAV(sound_path);
+        // if (s == nullptr)
+        // {
+        //     LOG_ERROR("Failed to load sound file: %s", SDL_GetError());
+        // }
+        // sounds.insert(std::pair<std::string, Mix_Chunk*>(sound_path, s));
     }
 
     void add_music(const char *music_path)
     {
-        auto m = Mix_LoadMUS(music_path);
-        if (m == nullptr)
-        {
-            LOG_ERROR("Failed to load music file: %s", SDL_GetError());
-        }
-        musics.insert(std::pair<std::string, Mix_Music*>(music_path, m));
+        LOG_ERROR("Audio not implemented");
+        // auto m = Mix_LoadMUS(music_path);
+        // if (m == nullptr)
+        // {
+        //     LOG_ERROR("Failed to load music file: %s", SDL_GetError());
+        // }
+        // musics.insert(std::pair<std::string, Mix_Music*>(music_path, m));
     }
 
     void play_sound(const char *sound_path, int volume)
     {
-        if (!sounds.contains(sound_path))
-        {
-            LOG_WARN("No sound found for: %s, attempting to add it", sound_path);
-            add_sound(sound_path);
-        }
-        auto channel = Mix_PlayChannel(-1, sounds[sound_path], 0);
-        Mix_Volume(channel, volume);
+        // if (!sounds.contains(sound_path))
+        // {
+        //     LOG_WARN("No sound found for: %s, attempting to add it", sound_path);
+        //     add_sound(sound_path);
+        // }
+        // auto channel = Mix_PlayChannel(-1, sounds[sound_path], 0);
+        // Mix_Volume(channel, volume);
+        LOG_ERROR("Audio not implemented");
     }
 
     void stop_sound(const char *sound_path)
     {
-        LOG_INFO("I haven't implemented this one oops");
+        LOG_ERROR("Audio not implemented");
     }
 
     void play_music(const char *music_path)
     {
-        if (!musics.contains(music_path))
-        {
-            LOG_ERROR("No music found for: %s", music_path);
-            return;
-        }
-        Mix_PlayMusic(musics[music_path], 9999);
+        // if (!musics.contains(music_path))
+        // {
+        //     LOG_ERROR("No music found for: %s", music_path);
+        //     return;
+        // }
+        // Mix_PlayMusic(musics[music_path], 9999);
+        LOG_ERROR("Audio not implemented");
     }
 
     void stop_music(const char *music_path)
     {
-        Mix_HaltMusic();
+       // Mix_HaltMusic();
+        LOG_ERROR("Audio not implemented");
     }
 
     void set_music_volume(int volume)
     {
-        Mix_VolumeMusic(volume);
+        // Mix_VolumeMusic(volume);
+        LOG_ERROR("Audio not implemented");
     }
 
     void destroy()
     {
-        for (const auto& music : musics)
-        {
-            Mix_FreeMusic(music.second);
-        }
-        for (const auto& sound : sounds)
-        {
-            Mix_FreeChunk(sound.second);
-        }
-        Mix_CloseAudio();
-        Mix_Quit();
+        // for (const auto& music : musics)
+        // {
+        //     Mix_FreeMusic(music.second);
+        // }
+        // for (const auto& sound : sounds)
+        // {
+        //     Mix_FreeChunk(sound.second);
+        // }
+        // Mix_CloseAudio();
+        // Mix_Quit();
+        LOG_ERROR("Audio not implemented");
     }
 }
